37ede4a181b80e0ca70040406e2fe7f3d8d792b7
Mior Muhammad Zaki <crynobone@gmail.com>
2020-01-25 12:03:00 +0800

    Add publish tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/MinionsServiceProviderTest.php
+++ tests/Feature/MinionsServiceProviderTest.php
@@ -2,6 +2,8 @@
 
 namespace Minions\Server\Tests\Feature;
 
+use Illuminate\Support\ServiceProvider;
+use Minions\Server\MinionsServiceProvider;
 use Orchestra\Testbench\TestCase;
 
 class MinionsServiceProviderTest extends TestCase
@@ -19,7 +21,7 @@ class MinionsServiceProviderTest extends TestCase
             \Laravie\Stream\Laravel\StreamServiceProvider::class,
             \Minions\MinionsServiceProvider::class,
             \Minions\Http\MinionsServiceProvider::class,
-            \Minions\Server\MinionsServiceProvider::class,
+            MinionsServiceProvider::class,
         ];
     }
 
@@ -29,5 +31,13 @@ class MinionsServiceProviderTest extends TestCase
         $this->assertSame('127.0.0.1', config('minions-server.host'));
         $this->assertSame(8085, config('minions-server.port'));
         $this->assertFalse(config('minions-server.secure'));
+    }
+
+    /** @test */
+    public function it_can_publish_configuration_file()
+    {
+        $this->assertSame([
+            realpath(__DIR__.'/../../').'/src/../config/minions-server.php' => $this->app->configPath('minions-server.php'),
+        ], ServiceProvider::pathsToPublish(MinionsServiceProvider::class, 'config'));
     }
 }
